15525dd6b41b46aa7fe1b798a8426da9a99af2a0
Raphaël Bobbe <74540137+raphaelbobbe@users.noreply.github.com>
2022-03-03 10:56:44 +0000

    10 producten en prijs plaats

--- db/create.sql
+++ db/create.sql
@@ -20,11 +20,20 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Goudvis', 'De Goudvis is een vis die niet mag ontbreken in uw aquaruim. De vis heeft een prachtige kleur en is makkelijk te verzorgen. Om deze rede is het ook een van de best verkochte vis uit onze winkel.', '816905633-0', 10.5);
+insert into products (name, description, code, price) values ('Goudvis', 'Goudvissen zijn de meest verkochte vissen die er zijn. Ze zwemmen voornamenlijk in het middelste deel van het aquarium en vinden het leuk als er beplanting is.', '077030122-3', 11);
 insert into products (name, description, code, price) values ('Guppy', 'Guppys komen oorspronkelijk uit Noord Amerika. de vis is tussen de 4 en de 6 cm groot. en ze houden graag van een warm aquarium. de vis houdt ervan om in een groep te zijn, en daarom adviseren wij ook om er meerdere in je aquarium te hebben.', '077030122-3', 11);
 insert into products (name, description, code, price) values ('Zwaarddrager', 'Zwaarddrager zijn net als Guppys, vissen die afkomstig zijn uit Noord Amerika. de vissen worden tussen de 6 en de 10 cm groot en ze houden van een warm en begroeid aquarium. De vissen voelen zich het meest op hun gemak in een school en dus raden wij aan er meerdere bij elkaar te zetten mocht je er een willen kopen.', '445924201-X', 13.5);
 insert into products (name, description, code, price) values ('Roodvinzalm', 'De Roodvinzalm is een kleine vis die erg actief is. De vis wordt ongeveer 5 cm groot. Hij zwemt voor namenlijk in het middelste en bovenste deel van je aquarium en daarom is hij veel zichtbaar. De vis voelt zich al snel comfortabel en het is ook niet perse nodig om er meerdere van in je aquarium te hebben.', '693155505-7', 13.5);
 insert into products (name, description, code, price) values ('Sumatraan', 'De Sumatraan heeft een erg mooie blauwe kleur gecombineerd met zwarte strepen. Hij is afkomstig uit Indonesie, en wordt gemmideld 7 cm groot. De vis voelt zich wel snel bedreigd en heeft daarom een schuilplaats nodig waar hij zich in kan terug trekken.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Platy', 'De Platy is een vis die afkomstig is uit midden Amerika. de vis is gemiddeld 6 cm groot en zwemt graag in warm water. Hij zwemt het liefst in een groep en daarom wordt deze vis per 2 verkocht.', '492662523-7');
- 
+insert into products (name, description, code, price) values ('Platy', 'De Platy is een vis die afkomstig is uit midden Amerika. de vis is gemiddeld 6 cm groot en zwemt graag in warm water. Hij zwemt het liefst in een groep en daarom wordt deze vis per 2 verkocht.', '492662523-7',10); 
 insert into products (name, description, code, price) values ('kogelvis', 'De kogel vis is een erg mooie maar ook erg agresieve vis. We raden daarom ook aan om deze vis alleen te kopen als je verstand hebt van vissen. als deze vis zich niet op zijn gemak voelt kan hij andere vissen op eten. Mocht je deze vis toch willen zet hem dan in een aquarium met voldoende ruimte en zonder andere vissen om hem heen.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('kegelvlekbarbeel', 'Deze vis is afkomstig uit zuid-oost Azie. De vis is relatief klein en hij zwemt voornamenlijk in het onderste deel van je aquarium. De vis voelt zich het fijnst als hij in een schol van 10 vissen is.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Zebravis', 'De Zebravis is net als de Kegelvlekbarbeel afkomstig uit zuid-oost Azie. De vis zwemt voornamenlijk in het middel/bovenste deel van het aquarium, en ook vindt hij het fijn als er veel licht is. Daarnaast vind de vis het fijn als er veel beplanting in het aquarium is.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Purperkop', 'Deze vis is afkomstig uit het zuiden van Sri Lanka. de vis houdt erg van beplanting en wordt ongeveer 6cm groot. HIj zwemt niet graag in een schol en dus kan je er prima 1 in je aquarium houden.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Maanvis Leopard', 'DE vis is afkomstig uit zuid-Amerika. Het eet graag diepvries voer en levend voer. De vis houdt niet van beplanting en zwemt ook niet graag in een schol.', '492662523-7', 14);
+
+
+
+
+
+  
